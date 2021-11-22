40ec07d97e47e458f9197ea94f5d42ccbf71f7d9
NincsJobbOtletem <patrikvok123312@gmail.com>
2021-11-22 18:46:39 +0100

    log basics

--- .nuxt/middleware.js
+++ .nuxt/middleware.js
@@ -1,3 +1,6 @@
 const middleware = {}
 
+middleware['log'] = require('../middleware/log.js')
+middleware['log'] = middleware['log'].default || middleware['log']
+
 export default middleware
--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-auth-page">
     <div class="auth-container">
-      <form>
+      <form @submit.prevent="onSubmit">
         <AppControlInput type="email">E-Mail Address</AppControlInput>
         <AppControlInput type="password">Password</AppControlInput>
         <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
